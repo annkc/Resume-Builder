3b2ac5158558a7f9d557177e3a133b678f8b2257
liangtam <tammieliang@hotmail.com>
2023-03-11 17:23:17 -0800

    Created backend. Starting frontend set up.

---
+++ backend/controllers/resumeController.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Resume = require('../models/resumeModel');
+
+// GET single resume
+const getSingleResume = async(req, res) => {
+    // this is getting the "/:id" from the link "/api/resumes/:id"
+    const {id} = req.params;
+
+    const resume = await Resume.findById(id);
+
+    // if the resume we're looking for DNE
+    if (!resume) {
+        return res.status(404).json({error: 'Resume does not exist'});
+    }
+
+    res.status(200).json(resume);
+}
+
+
+// GET all resumes
+const getResumes = async(req, res) => {
+
+    const resumes = await Resume.find({});
+    
+    res.status(200).json(resumes);
+}
+
+
+const postResume = async(req, res) => {
+    const {name, phoneNumber, email, website, education, objectives, workExperience, projects, extraCurricularActivities, awardsAndAchievements} = req.body;
+
+    try {
+        const resume = await Resume.create({name, phoneNumber, email, website, education, objectives, workExperience, projects, extraCurricularActivities, awardsAndAchievements});
+        res.status(200).json(resume);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
+
+module.exports = {getResumes, getSingleResume, postResume};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
-sdfhdsiohfid
+const express = require('express');
+const resumeRoutes = require('./routes/resumes');
+
+// now we're making an app
+const app = express();
+
+// getting mongo yay
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+// middleware
+app.use(express.json());
+
+// these are routes
+app.use('/api/resumes', resumeRoutes);
+
+// connecting to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+
+    app.listen(process.env.PORT, () => {
+        console.log("connected to database, on port ", process.env.PORT);
+    });
+
+})
+.catch((error) => {
+    console.log(error);
+});
+
