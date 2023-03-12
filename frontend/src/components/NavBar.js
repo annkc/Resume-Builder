c3754a2f156ad5bdb8eaa647b496588ead1c8c04
liangtam <tammieliang@hotmail.com>
2023-03-12 03:26:16 -0700

    Added CSS styling to some pages.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,13 +1,23 @@
+import {Link} from 'react-router-dom';
+import styles from './NavBar.module.css'
+
 export default function NavBar() {
-    return <nav className="nav">
-        <a href="/" className="site-name">Catch A Job</a>
-        <ul>
-            <li>
-                <a href="/home">Home</a>
-                <a href="/create-resume">Create</a>
-                <a href="/resumes">Resumes</a>
-                <a href="/about">About</a>
-            </li>
-        </ul>
-    </nav>
+    return (
+    <div className={styles.nav_container}>
+        <img src ="/images/Catch_a_job_logo_orange.png" alt=""/>
+        <div className={styles.site_title}>
+            <Link to="/">
+                <h1>Catch-A-Job</h1>
+            </Link>
+        </div>
+        <div className={styles.links}>
+        <li>
+            <ul><a href="/home">Home</a></ul>
+            <ul><a href="/create-resume">Create</a></ul>
+            <ul><a href="/resumes">Resumes</a></ul>
+            <ul><a href="/about">About</a></ul>
+        </li>
+        </div>
+    </div>
+    )
 }
