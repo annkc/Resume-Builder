99dca18755892f741290ded61e0af1dbae11a31a
Anneka Chen <chenanneka@gmail.com>
2023-03-11 20:13:30 -0800

    pages set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import About from './pages/About'
 import Home from './pages/Home'
 import ResumeBuilder from './pages/ResumeBuilder'
 import Resumes from './pages/Resumes'
+import NoPage from './pages/NoPage'
 
 function App() {
   return (
@@ -10,6 +11,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/create" element={<ResumeBuilder />} />
+          <Route path="/view" element={<Resumes />} />
+          <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/NoPage.js
@@ -0,0 +1,9 @@
+const NoPage = () => {
+    return (
+        <div className="noPage">
+            <h2>404: No page</h2>
+        </div>
+    )
+}
+
+export default NoPage;
