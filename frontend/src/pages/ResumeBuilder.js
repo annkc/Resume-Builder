32d4ed50f2c8a24cfa3ae5ec1b74272cb8bc5339
Anneka Chen <chenanneka@gmail.com>
2023-07-06 20:46:16 -0700

    centered creation page header

--- frontend/src/pages/ResumeBuilder.js
+++ frontend/src/pages/ResumeBuilder.js
@@ -1,8 +1,10 @@
 import Form from '../components/Form'
 
+import styles from './ResumeBuilder.module.css'
+
 const ResumeBuilder = () => {
     return (
-        <div className="resumeBuilder">
+        <div className={styles.resumeBuilder}>
             <h2>Create Your Resume</h2>
             <Form/>
         </div>
